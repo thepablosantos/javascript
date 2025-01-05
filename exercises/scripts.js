263328c04cb23075fb56c838794160566e605b2d
Pablo Santos <iphonepablosantos@gmail.com>
2025-01-05 14:28:16 +0000

    js

---
+++ exercises/scripts.js
@@ -0,0 +1,30 @@
+// 1 - exercise
+
+console.log("Pablo");
+console.log(`Pablo`);
+console.log(`My name is ${"Pablo"}`);
+
+// 2 - exercise
+
+console.log(7);
+console.log(7.1);
+console.log(7 + 7.7);
+console.log(7+7(12 * 2));
+
+// 3 - exercise
+
+console.log(7 > 6);
+console.log(7 < 6);
+console.log(7 != 7);
+console.log("Pablo" != "Joao");
+
+// 4 - exercise
+
+console.log(7 > 6 && 6 > 5);
+console.log(7 === 6 || 7 === "7");
+console.log(!true);
+console.log(!(2 === 2));
+
+// 5 - exercise
+
+console.log(7 * "Pablo");
--- section_2/scripts.js
+++ section_2/scripts.js
@@ -2,3 +2,33 @@ console.log("Hello World");
 console.log(5 / 2);
 console.log(typeof (5+3));
 console.log(typeof ("Hello"));
+console.log(typeof (true));
+
+// strings
+
+console.log("This is a test \nabout this string")
+console.log(`A soma de 2 + 2 é ${2+2}`);
+console.log(`Meu nome é: ${"Pablo"} e meu sobrenome é \n${"Santos"}`);
+console.log("salada" + " de" + " fruta");
+
+// booleans
+
+console.log(5 > 2);
+console.log(5 == 5);
+console.log(5 == 4);
+console.log(7 != 7);
+console.log(7 <= 7);
+console.log(7 < 6);
+console.log(true);
+console.log(false);
+console.log(7 == 7 || 7 > 8);
+console.log(7 === "7");
+console.log(7 === 7);
+console.log(7 === 7 && 7 === "7");
+console.log(!true);
+console.log(null);
+console.log(undefined);
+console.log("5" + 1);
+console.log("5" - 3);
+console.log(5 * null);
+console.log("dois" * "três");

9de2fa2be87aa41412edf8c54fdcb46610ff8a3d
Pablo Santos <iphonepablosantos@gmail.com>
2025-01-05 13:10:13 +0000

    js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-console.log(typeof 5);
-console.log(typeof 10);
-console.log(typeof 29);
-
-//
---
+++ section_2/scripts.js
@@ -0,0 +1,4 @@
+console.log("Hello World");
+console.log(5 / 2);
+console.log(typeof (5+3));
+console.log(typeof ("Hello"));

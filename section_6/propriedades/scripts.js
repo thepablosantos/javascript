a73a1ca487a914c5cfa69eee2199a0a8b146888d
Pablo Santos <iphonepablosantos@gmail.com>
2025-01-08 00:03:41 +0000

    js

--- section_6/propriedades/scripts.js
+++ section_6/propriedades/scripts.js
@@ -2,8 +2,16 @@ let eu = {
     nome: "Pablo",
     profissao: "Dev web3",
     idade: 31,
+    hello: function() {
+        console.log("Hello World!");
+    }
 }
 
-console.log(pessoa.idade);
-console.log(pessoa.profissao);
-console.log(pessoa.idade);
+console.log(eu.nome);
+console.log(eu.profissao);
+console.log(eu.idade);
+delete eu.idade;
+console.log(eu.idade);
+eu.genero = "Masculino";
+console.log(eu.genero);
+eu.hello();

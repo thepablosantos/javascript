76c5484c2feffdcde61d1bd43af54c90a62bd80a
Pablo Santos <iphonepablosantos@gmail.com>
2025-01-09 21:11:07 +0000

    dog

---
+++ section_10/04_class/scripts.js
@@ -0,0 +1,10 @@
+class cachorro {
+    constructor(raca, patas, cor) {
+        this.raca = raca;
+        this.patas = patas;
+        this.cor = cor;
+    }
+}
+
+let labrador = new cachorro('Labrador', 4, 'Amarelo');
+console.log(labrador);

68d2b879b8cc8aa20b38e9f6bb85767e763bb330
Pablo Santos <iphonepablosantos@gmail.com>
2025-01-09 13:16:29 +0000

    js

---
+++ section_10/01_metodos/scripts.js
@@ -0,0 +1,11 @@
+const cachorro = {
+    uivar: function() {
+        console.log("Auuuu");
+    },
+    rosnar: function() {
+        console.log("grrrrr");
+    }
+}
+
+console.log(cachorro.uivar);
+console.log(cachorro.rosnar);

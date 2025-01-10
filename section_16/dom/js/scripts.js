5205f24a897f11ffb0ab4c254aa46995a2a712da
Pablo Santos <iphonepablosantos@gmail.com>
2025-01-10 23:26:33 +0000

    dom

--- section_16/dom/js/scripts.js
+++ section_16/dom/js/scripts.js
@@ -1,3 +1,5 @@
-console.log(document.body);
+//console.log(document.body);
 console.log(document.getElementsByTagName('footer'));
 console.log(document.getElementById("titulo-principal"));
+console.log(document.getElementsByClassName('itens-novos'));
+console.log(document.querySelector('#container p'))

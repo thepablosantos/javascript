0ad0fd56fbdda1f3913f3d302be67fdb1eee17a7
Pablo Santos <iphonepablosantos@gmail.com>
2025-01-17 20:49:31 +0000

    destructuring

--- ES6/scripts.js
+++ ES6/scripts.js
@@ -8,3 +8,25 @@ const highNumbers = arr.filter((n) => {
 })
 
 console.log(highNumbers)
+
+// 4 map
+const products = [
+    {name: 'Camisa', price: 10.99, category: 'Roupas'},
+    {name: 'Chaleira Elétrica', price: 49.99, category: 'Eletro'},
+    {name: 'Fogão', price: 400, category: 'Eletro'},
+    {name: 'Calça jeans', price: 50.99, category: 'Roupas'},
+]
+
+products.map((product) => {
+    if(product.category === 'Roupas') {
+        product.onSale = true
+    }
+})
+
+console.log(products)
+
+// 6 destructuring
+const fruits = ["Maça", "Laranja", "Mamão"]
+
+const [f1, f2, f3] = fruits
+console.log(f1)

265a748c1bf982511829666d1fc4da80386df591
Pablo Santos <iphonepablosantos@gmail.com>
2025-01-17 20:57:09 +0000

    destructuring

--- ES6/scripts.js
+++ ES6/scripts.js
@@ -30,3 +30,21 @@ const fruits = ["Maça", "Laranja", "Mamão"]
 
 const [f1, f2, f3] = fruits
 console.log(f1)
+
+// 8 classe
+class Product {
+    constructor(name, price) {
+        this.name = name
+        this.price = price 
+    }
+
+    productWithDiscount(discount) {
+        return this.price * ((100 - discount) / 100)
+    }
+}
+
+const shirt = new Product ('Camisa gola v', 20)
+
+console.log(shirt.name)
+
+console.log(shirt.productWithDiscount(10))

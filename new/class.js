82edd578126c4cfae65571b01d0576b4faf70f15
Pablo Santos <iphonepablosantos@gmail.com>
2025-01-21 14:04:29 +0000

    js

--- new/class.js
+++ new/class.js
@@ -1,4 +1,6 @@
 /*
+
+=== if ===
 function ePaisagem(largura, altura) {
     if(largura > altura) {
         return true;
@@ -8,6 +10,7 @@ function ePaisagem(largura, altura) {
 
 console.log(ePaisagem(20,10));
 
+=== while ===
 
 function ePaisagem(largura, altura) {
     while(largura>altura) { 
@@ -17,10 +20,27 @@ function ePaisagem(largura, altura) {
 }
 
 console.log(ePaisagem(10, 20));
-*/
+
+=== template literals ===
 
 function ePaisagem(largura, altura) {
     return largura > altura ? true : false;
 }
 
 console.log(ePaisagem(1080, 1920));
+
+=== return only ===
+
+function ePaisagem(largura, altura) {
+    return largura > altura;
+}
+
+console.log(ePaisagem(1920, 1080));
+
+
+=== arrow function ===
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(1920, 1080));
+
+*/

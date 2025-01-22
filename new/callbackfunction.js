66820380b16f21d386317f236d9addf30c6a7d11
Pablo Santos <iphonepablosantos@gmail.com>
2025-01-22 20:31:43 +0000

    js

--- new/callbackfunction.js
+++ new/callbackfunction.js
@@ -1,18 +1,20 @@
+function rand(min = 1000, max = 3000) {
+    const num = Math.random() * (max - min) + min;
+    return Math.floor(num);
+}
 function f1() {
     setTimeout(function() {
         console.log('f1');
-    }, 1000);
+    }, rand());
 }
 
 function f2() {
-    console.log('f2');
-}
-
-function f3() {
-    console.log('f3');
+    setTimeout(function() {
+        console.log('f2');
+    }, rand());
+    
 }
 
 f1();
 f2();
-f3();
 console.log('Hello World!');

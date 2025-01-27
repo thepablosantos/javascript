03f56941a9b670510c1ccec4ded2d23bb83a8a18
Pablo Santos <iphonepablosantos@gmail.com>
2025-01-27 20:12:57 +0000

    class

---
+++ ES6/class.js
@@ -0,0 +1,9 @@
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
+
+const p1 = new Pessoa('Pablo', 'Santos');
+console.log(p1);
---
+++ ES6/filter.js
@@ -0,0 +1,3 @@
+const numbers = [1, 2, 3, 4];
+const numbersfilter = numbers.filter(num => num > 2);
+console.log(numbersfilter);

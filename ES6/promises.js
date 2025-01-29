a04911476cdf92f7fcbc11fdd32bfc69cf40d5fb
Pablo Santos <iphonepablosantos@gmail.com>
2025-01-29 19:45:32 +0000

    exer

--- ES6/promises.js
+++ ES6/promises.js
@@ -1,5 +1,39 @@
-function waiting(msg, time) {
-    setTimeout(() => {
-        console.log(msg);
-    }, tempo);
+
+function multiplicar(n1, n2) {
+    const resultado = n1*n2
+    console.log(`O valor de n1 x n2 é: ${resultado}!`)
 }
+
+multiplicar(10, 10);
+
+
+const dobro = x => x * 2;
+
+console.log(dobro(10));
+
+
+
+class Carro {
+    constructor(marca, ano) {
+        this.marca = marca;
+        this.ano = ano;
+    }
+
+    descrição() {
+        console.log(`Este carro é ${this.marca} do ano ${this.ano}.`)
+    }
+}
+
+const carro1 = new Carro("Fiat", 2000);
+carro1.descrição();
+
+
+
+class Calculadora {
+    constructor() {}
+
+    somar = (a, b) => a + b; // Arrow function corretamente definida na classe
+}
+
+const calc = new Calculadora();
+console.log(`O resultado da soma é: ${calc.somar(10, 10)}`); // Correto!
